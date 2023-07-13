ddb9f8f9c1949653550dd0b22509684a161a80bd
melvspace <amelvspace@gmail.com>
2023-07-13 23:23:29 +0300

    fix: fix background tiling

--- scripts/background_layer.gd
+++ scripts/background_layer.gd
@@ -3,14 +3,15 @@ extends CanvasLayer
 
 
 @export
+var offset_node: Control
+
+@export
 var tile_offset: Vector2 = Vector2.ZERO
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	
-	pass
+	if offset_node:
+		tile_offset = -offset_node.global_position
+	$background/checker_pattern.material.set_shader_parameter("tile_offset", tile_offset)
+	$background/checker_pattern.material.set_shader_parameter("custom_color", $background/checker_pattern.self_modulate)
+		
