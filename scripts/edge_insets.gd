a7a21a025f730e28b3d03b9498bf7345b18f0509
melvspace <amelvspace@gmail.com>
2023-07-15 15:22:49 +0300

    feat: enhance safe area experience

--- scripts/edge_insets.gd
+++ scripts/edge_insets.gd
@@ -1,18 +1,40 @@
+@tool
 extends Resource
 
 class_name EdgeInsets
 
 @export
-var left: float
+var left: float :
+	set(value):
+		if (left != value):
+			emit_changed()
+		left = value
 
 @export
-var top: float
+var top: float :
+	set(value):
+		if (top != value):
+			emit_changed()
+		top = value
 
 @export
-var right: float
+var right: float :
+	set(value):
+		if (right != value):
+			emit_changed()
+		right = value
 
 @export
-var bottom: float
+var bottom: float :
+	set(value):
+		if (bottom != value):
+			emit_changed()
+		bottom = value
+
+
+func _set(property, value):
+	print(get_property_list())
+	return false
 
 
 func _init(left := 0, top := 0, right := 0, bottom := 0):
@@ -31,7 +53,7 @@ static func from_rect_inside_rect(inner_rect: Rect2, outer_rect: Rect2):
 	var top = inner_rect.position.y - outer_rect.position.y
 	var right = outer_rect.size.x - left - inner_rect.size.x
 	var bottom = outer_rect.size.y - top - inner_rect.size.y
-	
+
 	return EdgeInsets.new(left, top, right, bottom)
 
 
@@ -42,9 +64,30 @@ func only_positive() -> EdgeInsets:
 		right if right > 0 else 0,
 		bottom if bottom > 0 else 0,
 	)
-	
-func divide(value: float):
-	return EdgeInsets.new(left / value, top / value, right / value, bottom / value)
-	
+
+static func divide(insets: EdgeInsets, value: float) -> EdgeInsets:
+	return EdgeInsets.new(
+		insets.left / value,
+		insets.top / value,
+		insets.right / value,
+		insets.bottom / value
+	)
+
+static func take_max(left: EdgeInsets, right: EdgeInsets) -> EdgeInsets:
+	return EdgeInsets.new(
+		max(left.left, right.left),
+		max(left.top, right.top),
+		max(left.right, right.right),
+		max(left.bottom, right.bottom),
+	)
+
+static func take_min(left: EdgeInsets, right: EdgeInsets) -> EdgeInsets:
+	return EdgeInsets.new(
+		min(left.left, right.left),
+		min(left.top, right.top),
+		min(left.right, right.right),
+		min(left.bottom, right.bottom),
+	)
+
 func _to_string():
 	return "(l: %s, t: %s, r: %s, b: %s)" % [left, top, right, bottom]
--- scripts/safe_area.gd
+++ scripts/safe_area.gd
@@ -5,28 +5,49 @@ extends MarginContainer
 class_name SafeArea
 
 @export
-var debug_insets := EdgeInsets.new(0, 47, 0, 34)
+var debug_insets := EdgeInsets.new(0, 47, 0, 34) :
+	set(value):
+		debug_insets = value
+		calculate()
 
 @export
-var minimum := EdgeInsets.zero
+var minimum := EdgeInsets.zero :
+	set(value):
+		minimum = value
+		calculate()
 
-@export
 var view_rect: Rect2i
-
-@export
 var _safe_area: Rect2i
-
-@export
 var view_padding: EdgeInsets
 
+var padding: EdgeInsets :
+	get:
+		return EdgeInsets.take_max(view_padding, minimum)
+
+
+
 func _ready():
+	debug_insets.changed.connect(calculate)
+	minimum.changed.connect(calculate)
 	resized.connect(calculate)
 	calculate()
+
+
+func _get_tool_buttons(): 
+	return [
+		{
+			call=calculate,
+			text="Recalculate",
+		}
+	]
 
 
 # TODO: divide by pixel density in parent
 # TODO: add MediaQuery Node with pixel density and insets
 func calculate():
+	if not get_viewport():
+		return
+		
 	# TODO: use get_tree().scale ?
 	var scale = ProjectSettings.get_setting("display/window/stretch/scale")
 	var editor_resolution = Vector2i(ProjectSettings.get_setting(
@@ -35,14 +56,18 @@ func calculate():
 	))
 
 	var view_size = editor_resolution if Engine.is_editor_hint() else get_viewport().size
-	
+
 	view_rect = Rect2i(Vector2i(0, 0), view_size)
 	_safe_area = DisplayServer.get_display_safe_area()
 	view_padding = debug_insets if Engine.is_editor_hint() and debug_insets else EdgeInsets.from_rect_inside_rect(_safe_area, view_rect).only_positive()
-	view_padding = view_padding.divide(scale)
-	
-	add_theme_constant_override("margin_left", max(view_padding.left, minimum.left))
-	add_theme_constant_override("margin_top", max(view_padding.top, minimum.top))
-	add_theme_constant_override("margin_right", max(view_padding.right, minimum.right))
-	add_theme_constant_override("margin_bottom", max(view_padding.bottom, minimum.bottom))
+	if not Engine.is_editor_hint():
+		view_padding = EdgeInsets.divide(view_padding, scale)
+
+	var padding = self.padding
+
+	add_theme_constant_override("margin_left", padding.left)
+	add_theme_constant_override("margin_top", padding.top)
+	add_theme_constant_override("margin_right", padding.right)
+	add_theme_constant_override("margin_bottom", padding.bottom)
+	queue_sort()
 
