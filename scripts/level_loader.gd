1b3f6a42510a69604efa6fae5db20dca6cc90d6e
melvspace <amelvspace@gmail.com>
2023-07-16 22:07:54 +0300

    feat: add level end popup

--- scripts/level_loader.gd
+++ scripts/level_loader.gd
@@ -9,9 +9,14 @@ var level_group: LevelGroup
 var _index := 0
 var index: int :
 	get: return _index
-	
+
 @export
 var level_spawn_parent: Node
+
+
+
+@export
+var ui_parent: Node
 
 @export
 var restart_button: BaseButton
@@ -19,28 +24,30 @@ var restart_button: BaseButton
 @export
 var go_button: BaseButton
 
-@export
-var continue_button: BaseButton
+var _success_popup := preload("res://prefabs/ui/level_end_popup.tscn")
 
 var current_level: PackedScene :
 	get: return level_group.get_level_scene(index)
 
 var _instantiated_scene_node: GameMode
 
+signal restarted
+signal failed_popup_waited(should_show_popup: bool)
+
 
 func _ready():
 	restart()
 	restart_button.pressed.connect(restart)
-	continue_button.pressed.connect(go_to_next_level)
-	
+
 	get_tree().root.content_scale_factor = DisplayServer.screen_get_scale() if not custom_window_scale else custom_window_scale
-	
+
 	var size_delta = get_window().size * (get_tree().root.content_scale_factor - 1)
 	get_window().size *= get_tree().root.content_scale_factor
 	get_window().position = get_window().position - Vector2i(size_delta / 2)
-	
+
 
 func restart():
+	failed_popup_waited.emit(false)
 	assert(level_group, "No Level Group provided. Check parameters.")
 
 	if _instantiated_scene_node:
@@ -54,17 +61,51 @@ func restart():
 		level_spawn_parent.add_child(_instantiated_scene_node)
 	else:
 		add_child(_instantiated_scene_node)
-	
-	continue_button.visible = false
+		
+	restarted.emit()
 
 
 func go_to_next_level():
-	assert(level_group.has_next_level(index))
+	if not level_group.has_next_level(index):
+		return
+
 	_index += 1
 
 	restart()
 
 
+func _do_from_popup(popup: LevelEndPopup, action: Callable):
+	popup.hide_popup()
+	await popup.exit_completed
+	popup.queue_free()
+	
+	action.call()
+
 func _handle_state_change(state: GameMode.State):
 	if state == GameMode.State.success:
-		continue_button.visible = level_group.has_next_level(index)
+		var popup: LevelEndPopup = _success_popup.instantiate()
+		ui_parent.add_child(popup)
+		popup.text = ["Yeah!", "Rock On!", "Got It!", "Well Done!", "Boo-ya!", "Huzzah!"].pick_random()
+		popup.show_popup()
+
+		popup.go_pressed.connect(_do_from_popup.bind(popup, go_to_next_level))
+		popup.retry_pressed.connect(_do_from_popup.bind(popup, restart))
+		
+	elif state == GameMode.State.failed:
+		get_tree().create_timer(2).timeout.connect(func(): failed_popup_waited.emit(true))
+		restarted.connect(func(): failed_popup_waited.emit(false))
+		
+		var should_show_popup = await failed_popup_waited
+		if not should_show_popup:
+			return
+		
+		var popup: LevelEndPopup = _success_popup.instantiate()
+		popup.text = ["Whoops!"].pick_random()
+		popup.actions = [LevelEndPopup.Action.home, LevelEndPopup.Action.retry]
+		
+		ui_parent.add_child(popup)
+		popup.show_popup()
+
+		popup.go_pressed.connect(_do_from_popup.bind(popup, go_to_next_level))
+		popup.retry_pressed.connect(_do_from_popup.bind(popup, restart))
+
---
+++ scripts/ui/level_end_popup.tres.gd
@@ -0,0 +1,67 @@
+class_name LevelEndPopup extends Control
+
+enum Action {
+	home,
+	go,
+	retry,
+}
+
+
+@export
+var text: String = "Yeah!" :
+	set(value):
+		if text != value:
+			%main_text.text = value
+
+		text = value
+
+@export
+var actions: Array[Action] = [Action.home, Action.go, Action.retry]
+
+@onready
+var _home_button := %buttons__home
+
+@onready
+var _go_button := %buttons__go
+
+@onready
+var _retry_button := %buttons__retry
+
+
+signal home_pressed
+signal go_pressed
+signal retry_pressed
+
+signal exit_completed
+signal enter_completed
+
+func _ready():
+	show_popup()
+	
+	if Action.home not in actions:
+		_home_button.visible = false
+	elif Action.go not in actions:
+		_go_button.visible = false
+	elif Action.retry not in actions:
+		_retry_button.visible = false
+
+	_home_button.pressed.connect(func(): home_pressed.emit())
+	_go_button.pressed.connect(func(): go_pressed.emit())
+	_retry_button.pressed.connect(func(): retry_pressed.emit())
+
+
+func show_popup():
+	$AnimationPlayer.play("level_end_popup_animation")
+	$AnimationPlayer.animation_finished.connect(_handle_animation_finished)
+
+
+func hide_popup():
+	$AnimationPlayer.play_backwards()
+
+
+func _handle_animation_finished(animation: StringName):
+	if animation == "level_end_popup_animation":
+		if $AnimationPlayer.current_animation_position == 1:
+			enter_completed.emit()
+		else:
+			exit_completed.emit()
