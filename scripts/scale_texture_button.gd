6692117d1dd404bd8e53c4d032241445caa806f3
melvspace <amelvspace@gmail.com>
2023-07-18 23:06:41 +0300

    feat: add animated buttons

--- scripts/scale_texture_button.gd
+++ scripts/scale_texture_button.gd
@@ -2,16 +2,22 @@
 class_name ScaleTextureButton extends TextureButton
 
 @export
-var texture_scale := Vector2.ONE 
+var texture_scale := Vector2.ONE
 
 
 # TODO: use current texture of button
 func _ready():
+	if not self.texture_normal:
+		return
+
 	self.ignore_texture_size = true
 	self.custom_minimum_size = self.texture_normal.get_size() * texture_scale
 	self.stretch_mode = TextureButton.STRETCH_SCALE
 
 func _process(delta):
+	if not self.texture_normal:
+		return
+
 	self.ignore_texture_size = true
 	self.custom_minimum_size = self.texture_normal.get_size() * texture_scale
 	self.stretch_mode = TextureButton.STRETCH_SCALE
---
+++ scripts/ui/circle_button.gd
@@ -0,0 +1,41 @@
+@tool
+extends ScaleTextureButton
+
+
+@export
+var main_texture: Texture2D :
+	get: return texture_normal
+	set(value):
+		texture_normal = value
+
+		if not is_node_ready():
+			await ready
+
+		$shadow.size = texture_normal.get_size() * texture_scale
+
+
+# TODO: use current texture of button
+func _ready():
+	if not Engine.is_editor_hint():
+		button_up.connect(func(): _handle_toggled(false))
+		button_down.connect(func(): _handle_toggled(true))
+
+	super._ready()
+
+
+var _is_pressed = false
+var _tween: Tween
+var _waiting: bool = false
+func _handle_toggled(is_pressed: bool) -> void:
+	if _is_pressed != is_pressed and not _waiting:
+		if _tween and _tween.is_running():
+			_tween.kill()
+
+		_tween = create_tween()
+		if is_pressed:
+			_tween.tween_property($shadow, "position", Vector2(0, 1), .075)
+		else:
+			_tween.tween_property($shadow, "position", Vector2(0, 4), .075)
+
+	_is_pressed = is_pressed
+
