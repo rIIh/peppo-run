4d696cd38505c41b978244a4832ecf7b9d5f6537
melvspace <amelvspace@gmail.com>
2023-07-27 21:45:10 +0300

    feat: add road WIP

--- scripts/objects/road.gd
+++ scripts/objects/road.gd
@@ -2,7 +2,7 @@
 extends Node2D
 
 @export
-var length: float = 968 / 4 :
+var length: float = 242:
 	set(value):
 		if (length == value): 
 			return
@@ -11,6 +11,15 @@ var length: float = 968 / 4 :
 		_update_nodes()
 
 
+@export
+var simulation_length: float = 242 :
+	set(value):
+		if (simulation_length == value): 
+			return
+		
+		simulation_length = value
+		_update_nodes()
+
 func _ready():
 	_update_nodes()
 
@@ -18,4 +27,4 @@ func _ready():
 func _update_nodes():
 	if is_inside_tree():
 		$road.size.y = length * 4
-		$road.position = Vector2($road.position.x, -length / 2)
+		$road.position = Vector2(-length / 2, $road.position.y)
